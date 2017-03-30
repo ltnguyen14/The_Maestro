bea63367c71a1bfa8c0e5fe8329480e7b1e5b4f7
ltnguyen14 <ltnguyen14@earlham.edu>
2017-03-29 23:01:41 -0400

    small fix

--- data/states/menus.py
+++ data/states/menus.py
@@ -1,5 +1,6 @@
 import pygame as pg
 from .. import tools, setup
+from .. import constants as c
 
 class Menu(object):
     def __init__(self):
@@ -7,6 +8,9 @@ class Menu(object):
         self.quit = False
         self.next = None
         self.previous = None
+        self.test_image = setup.GFX['rect_box']
 
     def update(self, screen, keys):
+        screen.fill(c.RED)
+        screen.blit(self.test_image, (0,0))
         pass
